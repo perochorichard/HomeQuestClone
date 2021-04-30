4db399e6dbe023ddaef5b6972b69ffc21962d343
perochorichard <perocho@sheridancollege.ca>
2021-04-29 21:52:39 -0400

    Changed LogPage to stateless widget.

--- lib/view/log_page.dart
+++ lib/view/log_page.dart
@@ -4,13 +4,11 @@ import 'package:home_quest_clone/view/log_segment.dart';
 /*
 log_page. The over-all page that displays the Log of current player-data
  */
-class LogPage extends StatefulWidget {
-  @override
-  _LogPageState createState() => _LogPageState();
-}
+class LogPage extends StatelessWidget {
+  final List<LogSegment> logSegments;
 
-class _LogPageState extends State<LogPage> {
-  List<LogSegment> logSegments = [];
+  LogPage({this.logSegments});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
